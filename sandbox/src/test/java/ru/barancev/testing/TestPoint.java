c0ee6938d55c30cf4a709971de4e92dc28ddecab
Git Gitko <recorder15715@outlook.com>
2020-02-18 13:07:13 -0800

    Some changes

---
+++ sandbox/src/test/java/ru/barancev/testing/TestPoint.java
@@ -0,0 +1,17 @@
+package ru.barancev.testing;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class TestPoint {
+
+
+    @Test
+    public void testDistance(){
+        Point p1 = new Point(10, 2);
+        Point p2 = new Point(1,1);
+        p1.distance4(p2);
+       // assert p1.distance4(p2)==9.055385138137417;
+        Assert.assertEquals(p1.distance4(p2),9.055385138137417);
+    }
+}
