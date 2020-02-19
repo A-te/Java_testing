ab99f35919b6aa7a0bd227705cf3c9b4b8c1b22b
Git Gitko <recorder15715@outlook.com>
2020-02-18 20:17:21 -0800

    HW3

--- sandbox/src/test/java/ru/barancev/testing/TestPoint.java
+++ sandbox/src/test/java/ru/barancev/testing/TestPoint.java
@@ -11,7 +11,7 @@ public class TestPoint {
         Point p1 = new Point(10, 2);
         Point p2 = new Point(1,1);
         p1.distance4(p2);
-       // assert p1.distance4(p2)==9.055385138137417;
+        //assert p1.distance4(p2)==9.055385138137417;
         Assert.assertEquals(p1.distance4(p2),9.055385138137417);
     }
 }
