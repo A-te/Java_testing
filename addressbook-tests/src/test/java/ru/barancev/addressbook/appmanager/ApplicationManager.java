37268249aeb905f1cd71d6627206025276d40da3
Git Gitko <recorder15715@outlook.com>
2020-02-28 13:20:16 -0800

    Задание №7: Научиться запускать тесты в разных браузерах

--- addressbook-tests/src/test/java/ru/barancev/addressbook/appmanager/ApplicationManager.java
+++ addressbook-tests/src/test/java/ru/barancev/addressbook/appmanager/ApplicationManager.java
@@ -3,7 +3,10 @@ package ru.barancev.addressbook.appmanager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.remote.BrowserType;
 
 import java.util.concurrent.TimeUnit;
 
@@ -14,13 +17,33 @@ public class ApplicationManager {
 
     WebDriver wd;
 
+    public ApplicationManager(String browser) {
+
+        this.browser = browser;
+    }
+
     private ContactHelper contactHelper;
     private SessionHelper sessionHelper;
     private NavigationHelper navigationHelper;
     private GroupHelper groupHelper;
+    String browser;
+
+
 
     public void init() {
-        wd = new FirefoxDriver();
+
+        //wd = new FirefoxDriver();
+        //String browser = BrowserType.FIREFOX;
+
+
+        if (browser.equals(BrowserType.FIREFOX)){
+            wd = new FirefoxDriver();
+        } else if (browser.equals(BrowserType.CHROME)) {
+            wd = new ChromeDriver();
+        }else if (browser.equals(BrowserType.IE)) {
+            wd = new InternetExplorerDriver();
+        }
+
         wd.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
         wd.get("http://localhost/addressbook/");
         contactHelper = new ContactHelper(wd);
--- addressbook-tests/src/test/java/ru/barancev/addressbook/tests/TestBase.java
+++ addressbook-tests/src/test/java/ru/barancev/addressbook/tests/TestBase.java
@@ -1,12 +1,15 @@
 package ru.barancev.addressbook.tests;
 
+import org.openqa.selenium.remote.BrowserType;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import ru.barancev.addressbook.appmanager.ApplicationManager;
 
 public class TestBase {
 
-    protected final ApplicationManager app = new ApplicationManager();
+    //protected final ApplicationManager app = new ApplicationManager();
+
+    protected final ApplicationManager app = new ApplicationManager(BrowserType.CHROME);
 
     @BeforeMethod(alwaysRun = true)
     public void setUp() throws Exception {
