3d21de04eb25b45bc33131e6c182cde5a0106827
Git Gitko <recorder15715@outlook.com>
2020-03-03 11:51:25 -0800

    Лекция 3.10. Проверка и обеспечение выполнения предусловий

--- addressbook-tests/src/test/java/ru/barancev/addressbook/tests/GroupDeletionTests.java
+++ addressbook-tests/src/test/java/ru/barancev/addressbook/tests/GroupDeletionTests.java
@@ -1,16 +1,20 @@
 package ru.barancev.addressbook.tests;
 
 import org.testng.annotations.Test;
+import ru.barancev.addressbook.model.GroupData;
 
 public class GroupDeletionTests extends TestBase {
 
 
-  @Test
-  public void testDeleteGroup() throws Exception {
-    app.getNavigationHelper().gotoGroupPage();
-    app.getGroupHelper().selectGroup();
-    app.getGroupHelper().deleteSelectedGroups();
-    app.getGroupHelper().returnToGroupPage();
-  }
+    @Test
+    public void testDeleteGroup() throws Exception {
+        app.getNavigationHelper().gotoGroupPage();
+        if (! app.getGroupHelper().isThereAGroup()) {
+            app.getGroupHelper().createGroup(new GroupData("test1", "test2", "test3"));
+        }
+        app.getGroupHelper().selectGroup();
+        app.getGroupHelper().deleteSelectedGroups();
+        app.getGroupHelper().returnToGroupPage();
+    }
 
 }
