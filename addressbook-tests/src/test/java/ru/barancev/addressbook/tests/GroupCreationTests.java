4b632c5066863c8084dbd43de555652a8874950a
Git Gitko <recorder15715@outlook.com>
2020-03-06 11:10:41 -0800

    Лекция 4.8. Поиск максимального элемента в коллекции

--- addressbook-tests/src/test/java/ru/barancev/addressbook/tests/GroupCreationTests.java
+++ addressbook-tests/src/test/java/ru/barancev/addressbook/tests/GroupCreationTests.java
@@ -5,6 +5,7 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.barancev.addressbook.model.GroupData;
 
+import java.util.HashSet;
 import java.util.List;
 
 
@@ -16,10 +17,26 @@ public class GroupCreationTests extends TestBase {
     app.getNavigationHelper().gotoGroupPage();
     List<GroupData> before = app.getGroupHelper().getGroupList();
     //int before = app.getGroupHelper().getGroupCount();
-    app.getGroupHelper().createGroup(new GroupData("test1", "test2", "test3"));
+
+    GroupData group = new GroupData("test1", "test2", "test3");
+    app.getGroupHelper().createGroup(group);
     List<GroupData> after = app.getGroupHelper().getGroupList();
     //int after = app.getGroupHelper().getGroupCount();
     Assert.assertEquals(after.size(),before.size() + 1);
+
+
+    int max = 0;
+    for (GroupData g : after) {
+      if (g.getId() > max){
+        max = g.getId();
+      }
+    }
+    group.setId(max);
+    before.add(group);
+
+    Assert.assertEquals(new HashSet<Object>(after), new HashSet<Object>(before));
+
+
   }
 
 }
