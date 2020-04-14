95b57d290c80e72609d43e476abef76733a6affc
Git Gitko <recorder15715@outlook.com>
2020-04-13 17:49:58 -0700

    Лекция 6.5. Загрузка тестовых данных из файла

--- addressbook-tests/src/test/java/ru/barancev/addressbook/tests/GroupCreationTests.java
+++ addressbook-tests/src/test/java/ru/barancev/addressbook/tests/GroupCreationTests.java
@@ -1,14 +1,15 @@
 package ru.barancev.addressbook.tests;
 
 
-import net.bytebuddy.agent.builder.AgentBuilder;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import ru.barancev.addressbook.model.GroupData;
 import ru.barancev.addressbook.model.Groups;
 
-import javax.management.DescriptorKey;
-
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
@@ -20,17 +21,30 @@ import static org.hamcrest.MatcherAssert.assertThat;
 public class GroupCreationTests extends TestBase {
 
   @DataProvider
-  public Iterator<Object[]> validGroups(){
-    List<Object[]> list = new ArrayList<>();
-    list.add(new Object[] {new GroupData().withName("test1").withHeader("header1").withFooter("footer1")});
-    list.add(new Object[] {new GroupData().withName("test2").withHeader("header2").withFooter("footer2")});
-    list.add(new Object[] {new GroupData().withName("test3").withHeader("header3").withFooter("footer3")});
+  public Iterator<Object[]> validGroups() throws IOException {
+    List<Object[]> list = new ArrayList<Object[]>();
+    BufferedReader reader = new BufferedReader(new FileReader(new File
+            ("src/test/resources/groups.csv")));
+    String line = reader.readLine();
+    while (line != null){
+      String[] split = line.split(";");
+      list.add(new Object[] {new GroupData().withName(split[0]).withHeader(split[1]).withFooter(split[2])});
+      line = reader.readLine();
+    }
+    return list.iterator();
+
+  //Лекция 6.5. Загрузка тестовых данных из файла
+//  public Iterator<Object[]> validGroups(){
+//    List<Object[]> list = new ArrayList<>();
+//    list.add(new Object[] {new GroupData().withName("test1").withHeader("header1").withFooter("footer1")});
+//    list.add(new Object[] {new GroupData().withName("test2").withHeader("header2").withFooter("footer2")});
+//    list.add(new Object[] {new GroupData().withName("test3").withHeader("header3").withFooter("footer3")});
 
 //    list.add(new Object[] {"test1", "header1","footer1"});
 //    list.add(new Object[] {"test2", "header2","footer2"});
 //    list.add(new Object[] {"test3", "header3","footer3"});
 
-    return list.iterator();
+//    return list.iterator();
   }
 
   @Test(dataProvider = "validGroups")
